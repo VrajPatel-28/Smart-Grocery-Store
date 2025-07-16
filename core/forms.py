b5ce52cfadff64a7527288272542f476bd7b76af
VrajPatel-28 <vrajtemps1@gmail.com>
2025-07-16 15:04:13 +0530

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Receipt
 
@@ -12,10 +14,6 @@ class ReceiptForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['purchase'].required = False
-
-from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
 
