3cbad70b60743ec460bdd644ef14665cde6b3b38
VrajPatel-28 <vrajtemps1@gmail.com>
2025-07-16 15:02:48 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from django.contrib import admin
 
 from .views import (
     AnalyticsView,
+    CustomLoginView,
     DashboardView,
     ProfileView,
     PurchaseCreateView,
@@ -16,19 +15,15 @@ from .views import (
     SignupView,
     update_email_view,
     update_password_view,
-    CustomLoginView
 )
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
-    path('admin/', admin.site.urls),
-    path('login/',
-          CustomLoginView.as_view(template_name='base.html'),
-         name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('update-email/', update_email_view, name='update_email'),
     path('update-password/', update_password_view, name='update_password'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('purchases/', PurchaseListView.as_view(), name='purchase_list'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('purchases/<int:pk>/',
@@ -37,6 +32,5 @@ urlpatterns = [
     path('purchases/add/', PurchaseCreateView.as_view(), name='purchase_add'),
     path("upload-receipt/", ReceiptUploadView.as_view(),
          name="upload_receipt"),
-    path("analytics/", AnalyticsView.as_view(), name="analytics"),
-    
+    path("analytics/", AnalyticsView.as_view(), name="analytics")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
